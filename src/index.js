6c8005e2091199596dce29421e540d2472ff0b93
Ana Cecilia Toribio <anctoribio@gmail.com>
2022-11-04 14:05:17 -0500

    mejoramiento en index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import validator from './validator.js';
 
-console.log(validator);
+//Código para eliminar o el "efecto" submit
+const formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", controlSubmit);
+
+function controlSubmit(formulario) {
+    formulario.preventDefault()
+}
+
+//alert seleccionaste visa o mastercard
+const marcaTarjeta = document.getElementById("marca");
+const visa = document.getElementById("VISA");
+// const masterCard = document.getElementById("MasterCard")
+marcaTarjeta.addEventListener("input", alerta);
+
+function alerta() {
+    if (visa.checked) {
+        alert("Seleccionaste VISA")
+    }
+    else {
+        alert("Seleccionaste MasterCard")
+    }
+}
+
+// llamar las funciones de validator y unir a la funcion validar
+const botonValidar = document.getElementById("VALIDAR");
+const numero = document.getElementById("CreditCard");
+const spanPalabra = document.getElementById("Palabra");
+
+botonValidar.addEventListener("click", function(){
+   let numberCard = parseInt(numero.value);
+
+   let myFunc = num => Number(num);
+  
+   var intArr = Array.from(String(numberCard), myFunc)
+
+   if (validator.isValid(intArr) == true){
+    spanPalabra.innerHTML = "VÁLIDA, puedes continuar con tu pago"
+    const numeroEnmascarado = validator.maskify(numero.value);
+    numero.value = numeroEnmascarado;
+   } else {
+    spanPalabra.innerHTML = "INVÁLIDA, revise sus datos"
+   }
+   validator.maskify(intArr);
+   
+   console.log(validator.maskify(intArr));
+
+})
+
+let botonLimpiar = document.getElementById("LIMPIAR");
+botonLimpiar.addEventListener("click", limpiar);
+
+function limpiar (){
+    location.reload();
+}
+
+// botonValidar.addEventListener("click", validator.maskify);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,60 @@
+// let numero = [CreditCard]
+// let numero = [4,5,5,7,8,8,0,4,8,0,9,0,4,7,0,9]
+
 const validator = {
-  // ...
-};
+  isValid: function (numero){
+    //obtener el numero para validader -> usuario ()
+    //let numero = [4,5,5,1,7,0,8,3,1,2,6,6,0,8,1,7]
+    // Código Luhn
+    let invertido = [ ];
+    // invertir el orden del numero de tarjeta
+    for (let i = 0; i <= (numero.length - 1); i++) {
+      invertido[i] = numero[(numero.length -1) - i];
+    }
+    // console.log("numero invertido" + invertido)
+    //let numberInvertido = number.reverse();
+    // buscar ubicación par y multiplicarlo por 2, en caso de sr mayor a 9 se resta 10 y se suma 1(suma de los dígitos)
+    let comprobacion = [];
+    for (let i=0; i < numero.length; i++){
+        if (i % 2 != 0) {
+            comprobacion[i] = invertido[i] * 2
+            if (comprobacion[i] > 9){
+                comprobacion[i] = (comprobacion[i]-10) + 1
+            }
+        }
+        else {
+          comprobacion[i] = invertido[i]
+        }
+    }
+    //Validación de la suma de los numeros en 10.
+    let suma = 0;
+    for (let i=0; i < numero.length; i++){
+        suma = suma + comprobacion[i]}
+    
+      if (suma % 10 == 0){
+        return true
+    }
+    else {
+        return false
+    }
+  },
+
+  maskify: function (numero){
+    let mascara = [];
+      for (var i = 0; i < numero.length; i++) {
+        if (i < numero.length - 4){
+         mascara[i]= "*"
+      }
+        else{
+        mascara[i] = numero[i]
+      }
+      }
+  return(mascara).join("");
+}
+
+}
+
+// console.log(validator.isValid())
+// console.log(validator.maskify())
 
 export default validator;
